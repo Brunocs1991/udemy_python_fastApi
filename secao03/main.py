54dbf958d7d5553e0d7b1a9108d67e3f2d9693dd
Brunocosta1991 <brunocosta.1991@hotmail.com>
2023-11-24 20:46:48 -0300

    Delete

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from fastapi import Response
 
 from models import Curso
 
@@ -40,6 +41,25 @@ async def post_curso(curso: Curso):
     cursos[next_id] = curso
     del curso.id
     return curso
+
+
+@app.put("/cursos/{curso_id}")
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}')
+
+
+@app.delete("/cursos/{curso_id}")
+async def delete_curso(curso_id:int):
+    if curso_id in cursos:
+        cursos.pop(curso_id)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}')
 
 
 if __name__ == '__main__':
