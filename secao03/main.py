d7a0558396f79b6718875a439177a7be3ff06674
Brunocosta1991 <brunocosta.1991@hotmail.com>
2023-11-24 19:32:02 -0300

    Tratando erros

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
 
 app = FastAPI()
 
@@ -23,12 +25,15 @@ async def get_cursos():
 
 @app.get("/cursos/{curso_id}")
 async def get_curso(curso_id: int):
-    curso = cursos[curso_id]
-    curso.update({"id": curso_id})
-    return curso
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
 
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True, workers=4)
