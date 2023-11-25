2358996481f3ab24b552e7fa3463caab34cf309e
Brunocosta1991 <brunocosta.1991@hotmail.com>
2023-11-25 15:32:17 -0300

    Injeção dependencia

--- secao03/main.py
+++ secao03/main.py
@@ -1,14 +1,26 @@
-from typing import Optional
+from time import sleep
+from typing import Optional, Any
 
+from fastapi import Depends
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi import Header
 from fastapi import Path
 from fastapi import Query
 from fastapi import Response
 from fastapi import status
-from fastapi import Header
 
 from models import Curso
+
+
+def fake_db():
+    try:
+        print("Abrindo conexão com o banco de dados...")
+        sleep(1)
+    finally:
+        print("Fechando conexão com o banco de dados...")
+        sleep(1)
+
 
 app = FastAPI()
 
@@ -27,13 +39,13 @@ cursos = {
 
 
 @app.get("/cursos")
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 
 @app.get("/cursos/{curso_id}")
-async def get_curso(
-        curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
+async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3),
+                    db: Any = Depends(fake_db)):
     try:
         curso = cursos[curso_id]
         return curso
@@ -42,7 +54,7 @@ async def get_curso(
 
 
 @app.post("/cursos", status_code=status.HTTP_201_CREATED)
-async def post_curso(curso: Curso):
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
     cursos[next_id] = curso
     del curso.id
@@ -50,7 +62,7 @@ async def post_curso(curso: Curso):
 
 
 @app.put("/cursos/{curso_id}")
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         cursos[curso_id] = curso
         del curso.id
@@ -60,7 +72,7 @@ async def put_curso(curso_id: int, curso: Curso):
 
 
 @app.delete("/cursos/{curso_id}")
-async def delete_curso(curso_id: int):
+async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         cursos.pop(curso_id)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -69,11 +81,12 @@ async def delete_curso(curso_id: int):
 
 
 @app.get("/calculadora")
-async def calculadora(a: int = Query(gt=5), b: int = Query(gt=10), x_geek: str = Header(default=None), c: Optional[int] = None):
+async def calculadora(a: int = Query(gt=5), b: int = Query(gt=10), x_geek: str = Header(default=None),
+                      c: Optional[int] = None):
     soma = a + b
     if c:
         soma = soma + c
-    print (f'X-GEEK: {x_geek}')
+    print(f'X-GEEK: {x_geek}')
     return {"resultado": soma}
 
 
