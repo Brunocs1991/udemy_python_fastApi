61d9fa403a4d3a817ac06a18ded236c8ff09c5bd
Brunocosta1991 <brunocosta.1991@hotmail.com>
2023-11-25 17:09:08 -0300

    Validação com pydantic

--- secao03_p1/models.py
+++ secao03_p1/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class Curso(BaseModel):
@@ -8,6 +8,16 @@ class Curso(BaseModel):
     titulo: str
     aulas: int
     horas: int
+
+    @field_validator('titulo')
+    def validar_titulo(cls, value: str):
+        palavras = value.split(' ')
+        if len(palavras) < 3:
+            raise ValueError('O titulo deve ter pelo menos 3 palavras.')
+
+        if value.islower():
+            raise ValueError('O titulo deve ser capitalizado')
+        return value
 
 
 cursos = [
