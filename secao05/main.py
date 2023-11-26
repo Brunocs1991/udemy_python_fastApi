1d069b9027422a49b63a08a577f605ca779efe9a
Brunocosta1991 <brunocosta.1991@hotmail.com>
2023-11-26 18:44:10 -0300

    Projeto base

---
+++ secao05/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, log_level='info', reload=True)
